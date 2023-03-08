5159114e27910150f6b45978a1ccea3256ce68dc
Tux Mendoza <tux.mendoza@gmail.com>
2023-03-07 22:31:02 -0600

    Clase11

--- bin/codigo_curso1.dart
+++ bin/codigo_curso1.dart
@@ -1,17 +1,26 @@
 /*
- +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
- |T|i|p|o|s| |d|e| |d|a|t|o|s| |-| |B|o|o|l|e|a|n|o|s| |y| |S|t|r|i|n|g|s|
- +-+-+-+-+-+-+-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
- |C|l|a|s|e|0|9|
+ +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+
+ |T|i|p|o|s| |d|e| |d|a|t|o|s| |-| |L|i|s|t|a|
+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+
+ |C|l|a|s|e|1|1|
  +-+-+-+-+-+-+-+
 */
 
 void main(List<String> arguments) {
-  bool? isActive = null;
+  //Lista coleccion e elementos que tienen algun tipo de dato en comun
 
-  if (isActive == null) {
-    print('isActive es null');
-  } else {
-    print('No es null');
-  }
+  //lista vacia
+  List listaVacia = [];
+
+  //Lista inicializada simple
+  List listaSimple = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  listaSimple.add(11);
+  //El listado de en dart es en base cero,
+  //El inidice de las listas inicia en cero
+  //listaSimple[0]
+
+  //Sgregamos un string a la lista
+  listaSimple.add('Tux');
+
+  print(listaSimple);
 }
