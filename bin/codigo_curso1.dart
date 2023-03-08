54b7f0dab9957660680a82f5d3f3e91513935ad6
Tux Mendoza <tux.mendoza@gmail.com>
2023-03-07 21:18:47 -0600

    clase09 Tipos de datos, Numeros y Strings

--- bin/codigo_curso1.dart
+++ bin/codigo_curso1.dart
@@ -1,11 +1,30 @@
 /*
- +-+-+-+-+-+-+-+-+-+-+-+-+ +-+ +-+-+-+-+
- |i|n|t|r|o|d|u|c|c|i|o|n| |a| |D|a|r|t|
- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+ +-+-+-+-+
- |#| |8| |H|o|l|a| |M|u|n|d|o|
- +-+ +-+ +-+-+-+-+ +-+-+-+-+-+
+ +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
+ |T|i|p|o|s| |d|e| |d|a|t|o|s| |-| |N|u|m|e|r|o|s| |y| |S|t|r|i|n|g|s|
+ +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
+ |C|l|a|s|e| |0|9|
+ +-+-+-+-+-+ +-+-+
 */
 
 void main(List<String> arguments) {
-  print('Hello world!');
+  //Strings
+  var nombre = 'Tony';
+  var apellido = 'Stark';
+
+// Formas validas de inicializar una variable
+
+  //final String nombre = 'Peter';
+  //const apellido = 'Stark';
+
+  print('$nombre $apellido');
+
+//Numeros
+
+  //Entero
+  int empleados = 10;
+  //Decimal
+  double salario = 1856.25;
+
+  print(empleados);
+  print(salario);
 }
