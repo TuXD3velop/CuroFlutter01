153ab841abc84679de4a58571e87a08c6e6f712d
Tux Mendoza <tux.mendoza@gmail.com>
2023-03-07 21:43:49 -0600

    Clase10 Tipo de datos - Booleanos y condiciones

--- bin/codigo_curso1.dart
+++ bin/codigo_curso1.dart
@@ -1,30 +1,17 @@
 /*
- +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
- |T|i|p|o|s| |d|e| |d|a|t|o|s| |-| |N|u|m|e|r|o|s| |y| |S|t|r|i|n|g|s|
- +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
- |C|l|a|s|e| |0|9|
- +-+-+-+-+-+ +-+-+
+ +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
+ |T|i|p|o|s| |d|e| |d|a|t|o|s| |-| |B|o|o|l|e|a|n|o|s| |y| |S|t|r|i|n|g|s|
+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+
+ |C|l|a|s|e|0|9|
+ +-+-+-+-+-+-+-+
 */
 
 void main(List<String> arguments) {
-  //Strings
-  var nombre = 'Tony';
-  var apellido = 'Stark';
+  bool? isActive = null;
 
-// Formas validas de inicializar una variable
-
-  //final String nombre = 'Peter';
-  //const apellido = 'Stark';
-
-  print('$nombre $apellido');
-
-//Numeros
-
-  //Entero
-  int empleados = 10;
-  //Decimal
-  double salario = 1856.25;
-
-  print(empleados);
-  print(salario);
+  if (isActive == null) {
+    print('isActive es null');
+  } else {
+    print('No es null');
+  }
 }
