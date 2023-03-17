ce23a8530daa519e51a041e465277050a852cfb6
Tux Mendoza <tux.mendoza@gmail.com>
2023-03-16 21:41:04 -0600

    Clase12 Tipo de dato MAP

--- bin/codigo_curso1.dart
+++ bin/codigo_curso1.dart
@@ -1,34 +1,31 @@
 /*
- +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+
- |T|i|p|o|s| |d|e| |d|a|t|o|s| |-| |L|i|s|t|a|
- +-+-+-+-+-+-+-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+
- |C|l|a|s|e|1|1|
+ +-+-+-+-+-+ +-+-+ +-+-+-+-+-+ +-+ +-+-+-+
+ |T|i|p|o|s| |d|e| |d|a|t|o|s| |-| |M|a|p|
+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+ +-+ +-+-+-+
+ |C|l|a|s|e|1|2|
  +-+-+-+-+-+-+-+
 */
 
-void main(List<String> arguments) {
-  //Lista coleccion e elementos que tienen algun tipo de dato en comun
+void main() {
+  Map persona = {
+    'nombre': 'Tux',
+    'edad': 35,
+    'soltero': false,
+    true: false,
+    1: 100,
+    2: 500,
+  };
+  //La variable tipo Map la llave debe apuntar exactamente como esrta declarada
+  //en el mapa, asi sea un numero, string u objeto
 
-  //lista vacia
-  List listaVacia = [];
+// Se puede agregar un generico a la declaracion de la variable para forzar
+//El tipo de dato que acepta, tanto de al llave como del valor
+  Map<String, dynamic> persona2 = {
+    'nombre': 'Tux',
+    'edad': 35,
+    'soltero': false,
+  };
 
-  //Lista inicializada simple
-  List listaSimple = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  listaSimple.add(11);
-  //El listado de en dart es en base cero,
-  //El inidice de las listas inicia en cero
-  //listaSimple[0]
-
-  //Sgregamos un string a la lista
-  listaSimple.add('Tux');
-
-  //A la definicion del tipo de dato que contiene la lista
-  //se le llama generico y va encerrado entre <TipoDeDato>
-  List<int> listaConTipoGenerico = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  listaConTipoGenerico.add(11);
-
-  //Constructor generador de listas
-  final masNumeros = List.generate(100, (index) => 10);
-
-  print(masNumeros);
+  persona2.addAll({'segundoNombre': 'Juan'});
+  print(persona2);
 }
